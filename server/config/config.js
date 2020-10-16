91e4fdd883a7e900d980dfc064c160d4dc3a8a36
John Baxter <jbaxter@a-zdarts.com>
2020-10-15 19:49:26 -0700

    Trying to add jawsdb

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,18 @@
 require('dotenv').config()
 
-module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: 'weedmates_db',
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'weedmates_db',
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: 'weedmates_db',
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-  }
-}
+var mysql = require('mysql');
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: 'weedmates_db',
+      host: process.env.DB_HOST,
+      dialect: 'mysql'
+    })
+};

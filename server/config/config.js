52138503501ea2d47b3bd134e44846a1fcef4bc9
John Baxter <jbaxter@a-zdarts.com>
2020-10-17 22:05:31 -0700

    Changed server config to use jaws_db env

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,22 @@
 require('dotenv').config()
 
-var mysql = require('mysql');
-
-var connection;
-if (process.env.JAWSDB_URL) {
-    // Database is JawsDB on Heroku
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    // Database is local
-    connection = mysql.createConnection({
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: 'weedmates_db',
-      host: process.env.DB_HOST,
-      dialect: 'mysql'
-    })
-};
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'weedmates_db',
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'weedmates_db',
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: 'mysql'
+  }
+}
